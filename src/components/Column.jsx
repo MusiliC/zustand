4e80a6dc54ca1ff717af817cbf4acb6aa333ab4c
brian.musili <brian.musili@systechafrica.com>
2025-02-28 06:14:11 +0300

    wip: connect store and zustand

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,9 +1,28 @@
-import './Column.css'
+import { useStore } from "../../store";
+import "./Column.css";
+import Task from "./Task";
+import { useMemo } from "react";
 
-function Column({state}) {
+function Column({ state }) {
+  const tasks = useStore((store) => store.tasks);
+
+  const filteredTasks = useMemo(() => {
+    return tasks.filter((task) => task.state === state);
+  }, [tasks, state]);
+
+  const addTask = useStore((store) => store.addTask);
+
   return (
-    <div className='column'>{state}</div>
-  )
+    <div className="column">
+      <div className="titleWrapper">
+        <p>{state}</p>
+        <button onClick={() => addTask("test " + state, state)}>Add</button>
+      </div>
+      {filteredTasks.map((task) => (
+        <Task title={task.title} key={task.title} />
+      ))}
+    </div>
+  );
 }
 
-export default Column
+export default Column;
