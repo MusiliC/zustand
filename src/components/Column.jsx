c8be7c376d28fcb69999a331ed7b1c5f320d50b5
brian.musili <brian.musili@systechafrica.com>
2025-03-05 06:07:28 +0300

    wip: add more store actions, drag and drop tasks

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,10 +1,17 @@
+import classNames from "classnames";
 import { useStore } from "../../store";
 import "./Column.css";
 import Task from "./Task";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 
 function Column({ state }) {
   const tasks = useStore((store) => store.tasks);
+
+  const [text, setText] = useState("");
+
+  const [open, setOpen] = useState(false);
+
+  const [drop, setDrop] = useState(false);
 
   const filteredTasks = useMemo(() => {
     return tasks.filter((task) => task.state === state);
@@ -12,15 +19,56 @@ function Column({ state }) {
 
   const addTask = useStore((store) => store.addTask);
 
+  const setDraggedTask = useStore((store) => store.setDraggedTask);
+
+  const draggedTask = useStore((store) => store.draggedTask);
+
+  const moveTask = useStore((store) => store.moveTask);
+
   return (
-    <div className="column">
+    <div
+      className={classNames("column", {drop: drop})}
+      onDragOver={(e) => {
+        e.preventDefault();
+        setDrop(true);
+      }}
+      onDragLeave={(e) => {
+        e.preventDefault();
+        setDrop(false);
+      }}
+      onDrop={(e) => {
+        setDrop(false)
+        setDraggedTask(null);
+        moveTask(draggedTask, state);
+      }}
+    >
       <div className="titleWrapper">
         <p>{state}</p>
-        <button onClick={() => addTask("test " + state, state)}>Add</button>
+        <button onClick={() => setOpen(true)}>Add</button>
       </div>
       {filteredTasks.map((task) => (
         <Task title={task.title} key={task.title} />
       ))}
+      {open && (
+        <div className="Modal">
+          <div className="modalContent">
+            <input
+              type="text"
+              onChange={(e) => setText(e.target.value)}
+              value={text}
+            />
+            <button
+              onClick={() => {
+                addTask(text, state);
+                setText("");
+                setOpen(false);
+              }}
+            >
+              Add
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,21 +2,25 @@ import classNames from "classnames";
 import "./Task.css";
 import { useStore } from "../../store";
 
-
 function Task({ title }) {
+  const task = useStore((store) =>
+    store.tasks.find((task) => task.title === title)
+  );
 
-    const task = useStore(store => store.tasks.find(task => task.title === title))
+  const deleteTask = useStore((store) => store.deleteTask);
+
+  const setDraggedTask = useStore(store => store.setDraggedTask)
 
   return (
-    <div className="task">
+    <div className="task" draggable onDragStart={setDraggedTask(task.title)}>
       <div> {task.title}</div>
 
       <div className="bottomWrapper">
-        <div></div>
+        <div>
+          <button onClick={() => deleteTask(task.title)}>Delete</button>
+        </div>
         <div className={classNames("status", task.state)}>{task.state}</div>
       </div>
-      {/* 
-      <Task title="Todo" /> */}
     </div>
   );
 }
